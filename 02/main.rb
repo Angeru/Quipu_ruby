d0f12b77cbb66922cbf588d2a151f72893adf1c2
Angel Nieto <angellnieto@gmail.com>
2021-06-03 21:06:41 +0200

    2-a refactor

--- 02/main.rb
+++ 02/main.rb
@@ -1,10 +1,17 @@
+def extract_data line
+    data = line.split(" ")
+    {
+        :start => data[0].split("-").first.to_i,
+        :end => data[0].split("-").last.to_i,
+        :letter => data[1].gsub(":",""),
+        :pass => data[2]
+    }
+end
+
 count=0
 File.open( "input.txt").each do |line|
-    data = extract_data(line.chomp)
-    start = data[0].split("-").first.to_i
-    endd = data[0].split("-").last.to_i
-    letter = data[1].gsub(":","")
-    ocurrences = data[2].count(letter)
-    count+=1 if ocurrences >= start && ocurrences <= endd
+    data = extract_data(line.chomp)   
+    ocurrences = data[:pass].count(data[:letter])
+    count+=1 if ocurrences >= data[:start] && ocurrences <= data[:end]
 end
 puts count
